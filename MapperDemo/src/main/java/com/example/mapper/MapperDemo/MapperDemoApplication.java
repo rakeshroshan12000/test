5c5d1d68f9ef02fd2b7b55d6f0090e0d86fbfc80
rakesh roshan <rakeshroshan44@gmail.com>
2021-03-31 13:39:05 +0530

    changes for consul test

--- MapperDemo/src/main/java/com/example/mapper/MapperDemo/MapperDemoApplication.java
+++ MapperDemo/src/main/java/com/example/mapper/MapperDemo/MapperDemoApplication.java
@@ -2,8 +2,10 @@ package com.example.mapper.MapperDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MapperDemoApplication {
 
 	public static void main(String[] args) {
--- MapperDemo/src/main/java/com/example/mapper/MapperDemo/controllers/TestApiController.java
+++ MapperDemo/src/main/java/com/example/mapper/MapperDemo/controllers/TestApiController.java
@@ -1,16 +1,38 @@
 package com.example.mapper.MapperDemo.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @Validated
 @RequiredArgsConstructor
 public class TestApiController {
+
     private final ApiMapper mappers;
+
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+
     @GetMapping("/test")
     public TestDTO assignUserPlan(){
         return mappers.map("Success");
+    }
+
+//    public Optional<URI> serviceUrl() {
+//        return discoveryClient.getInstances("MapperDemo")
+//                .stream()
+//                .findFirst()
+//                .map(si -> si.getUri());
+//    }
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
     }
 }
