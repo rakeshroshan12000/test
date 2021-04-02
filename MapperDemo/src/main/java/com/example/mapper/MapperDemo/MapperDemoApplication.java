7b452f7eea514a694ef4ae9d492aae3cc4f04237
rakesh roshan <rakeshroshan44@gmail.com>
2021-04-02 10:56:04 +0530

    changes for the consul

--- MapperDemo/src/main/java/com/example/mapper/MapperDemo/MapperDemoApplication.java
+++ MapperDemo/src/main/java/com/example/mapper/MapperDemo/MapperDemoApplication.java
@@ -1,11 +1,15 @@
 package com.example.mapper.MapperDemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties
 public class MapperDemoApplication {
 
 	public static void main(String[] args) {
--- MapperDemo/src/main/java/com/example/mapper/MapperDemo/controllers/TestApiController.java
+++ MapperDemo/src/main/java/com/example/mapper/MapperDemo/controllers/TestApiController.java
@@ -2,23 +2,30 @@ package com.example.mapper.MapperDemo.controllers;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.util.Optional;
 
+@RefreshScope
 @RestController
 @Validated
 @RequiredArgsConstructor
 public class TestApiController {
 
+    @Value("${msg}")
+    private String message;
+
     private final ApiMapper mappers;
 
     @GetMapping("/test")
     public TestDTO assignUserPlan(){
-        return mappers.map("Success");
+        return mappers.map(message);
     }
 
     @GetMapping("/ping")
